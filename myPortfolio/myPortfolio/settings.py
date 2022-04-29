3a599db852d0697164fffea33a849e58d43d07fb
sharath.hg <sharath.hg@teams.unvired.com>
2022-04-29 16:23:24 +0530

    added background animation

--- myPortfolio/myPortfolio/settings.py
+++ myPortfolio/myPortfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a-1$-+y!t1*oe8g-z(a4b8e#sr5j5m00v=xe$i+wxv6g^p&$14
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sharu.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
